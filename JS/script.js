b02b4e3b503f9c031e7cf6cafb29253b71e588ba
Lluis Belmonte <lbelmon4@xtec.cat>
2025-11-09 16:10:55 +0100

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -58,6 +58,34 @@ if (true) {
 }
 // console.log(preu); Error: preu no està definit fora del bloc
 
+//OBJECTES amb let
+let cotxe = {
+    marca: "Toyota",
+    model: "Corolla",
+    any: 2020
+};
+
+// Accedir
+console.log(cotxe.marca);
+
+// Modificar
+cotxe.any = 2023;
+
+// Afegir propietat
+cotxe.color = "Blau";
+
+// Sobreescriure tot l'objecte
+cotxe = {
+    marca: "Honda",
+    model: "Civic",
+    any: 2022
+};
+
+// Eliminar propietat
+delete cotxe.model;
+
+console.log(cotxe);
+
 /*------------------------------CONST (valors constants)------------------------------*/
 const PI = 3.1416;
 console.log(PI);
@@ -77,3 +105,24 @@ alumne.edat = 21; // Pots modificar una propietat
 console.log(alumne); // { nom: "Júlia", edat: 21 }
 
 // alumne = {}; No pots reassignar l’objecte sencer
+
+/*------------------------------VARIABLES------------------------------*/
+//nom descriptius i correctes
+let nom4;      // correcte
+let _edat;    // correcte
+let $sou;     // correcte
+//let 1valor;   // incorrecte, no pot començar amb un número
+
+let nom1;        // correcte
+let edat_2025;   // correcte
+let $total_sum;  // correcte
+//let percentatge%;// incorrecte, el % no és permès
+
+//let for;    // incorrecte
+//let const;  // incorrecte
+let suma;   // correcte
+
+let variable = 10;
+let Variable = 20; 
+console.log(variable); // 10
+console.log(Variable); // 20
